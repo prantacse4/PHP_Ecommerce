7702a1c97e0e2d4ab45ce331c1d888eb6af33da8
Pranta Biswas <34433419+prantacse4@users.noreply.github.com>
2020-09-22 13:23:15 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+if(session_id() == '' || !isset($_SESSION)){session_start();}
+?>
+
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>PHOTOGRAPHY PRODUCTS SHOP</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+  </head>
+  <body>
+
+    <nav class="top-bar" data-topbar role="navigation">
+      <ul class="title-area">
+        <li class="name">
+          <h1><a href="index.php">PHOTOGRAPHY PRODUCTS SHOP</a></h1>
+        </li>
+        <li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
+      </ul>
+
+      <section class="top-bar-section">
+      <!-- Right Nav Section -->
+        <ul class="right">
+          <li><a href="about.php">About</a></li>
+          <li><a href="products.php">Products</a></li>
+          <li><a href="cart.php">View Cart</a></li>
+          <li><a href="orders.php">My Orders</a></li>
+          <li><a href="contact.php">Contact</a></li>
+          <?php
+
+          if(isset($_SESSION['username'])){
+            echo '<li><a href="account.php">My Account</a></li>';
+            echo '<li><a href="logout.php">Log Out</a></li>';
+          }
+          else{
+            echo '<li><a href="login.php">Log In</a></li>';
+            echo '<li><a href="register.php">Register</a></li>';
+          }
+          ?>
+        </ul>
+      </section>
+    </nav>
+
+    <img src="images/bolt-landscape.jpg">
+
+
+    <div class="row" style="margin-top:10px;">
+      <div class="small-12">
+
+        <footer style="margin-top:10px;">
+           <p style="text-align:center; font-size:0.8em;">&copy; PHOTOGRAPHY PRODUCTS SHOP. All Rights Reserved.</p>
+        </footer>
+
+      </div>
+    </div>
+
+
+
+  </body>
+</html>
